0730d476f7cec426985e3f4342569fe18b6a9e46
Abhishek Kumar <avykr07@gmail.com>
2024-03-19 19:26:00 +0530

    solved

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,13 @@
-import React from 'react';
-import { useRouter } from 'next/router';
+import React from "react";
+import { useRouter } from "next/router";
+import { useParams } from "next/navigation";
 
 function PostDetail() {
-  // Complete the function
+	const params = useParams();
+	const { id } = params;
+	console.log(id);
+	// Complete the function
+	return <h1>Post Detail: {id}</h1>;
 }
 
 export default PostDetail;
