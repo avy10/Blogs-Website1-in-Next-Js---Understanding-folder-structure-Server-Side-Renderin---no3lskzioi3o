ed5382d988cb919f5b46d77489011ee3094af697
Abhishek Kumar <avykr07@gmail.com>
2024-03-19 19:31:50 +0530

    fixing last test case

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,9 +3,11 @@ import { useRouter } from "next/router";
 import { useParams } from "next/navigation";
 
 function PostDetail() {
-	const params = useParams();
-	const { id } = params;
-	console.log(id);
+	// const params = useParams();
+	// const { id } = params;
+	// console.log(id);
+	const router = useRouter();
+	const { id } = router.query;
 	// Complete the function
 	return <h1>Post Detail: {id}</h1>;
 }
